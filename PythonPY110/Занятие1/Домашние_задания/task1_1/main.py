1d2c6afb66186477bd4e6984dab63cc7ef6ef2fb
RinaEastwind <RinaEastwind@users.noreply.github.com>
2023-04-23 17:08:44 +0500

    23/04

--- PythonPY110/Занятие1/Домашние_задания/task1_1/main.py
+++ PythonPY110/Занятие1/Домашние_задания/task1_1/main.py
@@ -1,6 +1,8 @@
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    def my_enumerate(lst:list):
+        my_enu = zip(range(0, len(lst)), lst)
+        return my_enu
 
-
-...
+    # a = [15, 5, 6, 4, 8, 79, 16, 3, 0, 3, 5]
+    # for x, y in my_enumerate(a):
+    #     print(x, y)
