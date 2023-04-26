8aa048dfe065a09dd758003741986e9b2f8e97c8
RinaEastwind <RinaEastwind@users.noreply.github.com>
2023-04-26 12:50:00 +0500

    26/04

--- PythonPY110/Занятие3/Лабораторные_задания/task1_4/main.py
+++ PythonPY110/Занятие3/Лабораторные_задания/task1_4/main.py
@@ -2,8 +2,10 @@ INPUT_FILE = "input.txt"
 OUTPUT_FILE = "output.txt"
 
 
-def task():
-    ...  # TODO перезаписать содержимое одного файла в другой
+def task():# TODO перезаписать содержимое одного файла в другой
+    with open(INPUT_FILE, "r") as inpuut,\
+        open(OUTPUT_FILE, "w") as outpuut:
+        outpuut.write(''.join(x for x in map(str.upper, inpuut)))
 
 
 if __name__ == "__main__":
