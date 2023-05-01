686635ce77b40981de3743c5feb02203f0d63e61
RinaEastwind <RinaEastwind@users.noreply.github.com>
2023-05-01 22:43:41 +0500

    26/04

--- PythonPY110/Занятие3/Лабораторные_задания/task2_1/main.py
+++ PythonPY110/Занятие3/Лабораторные_задания/task2_1/main.py
@@ -2,8 +2,9 @@ import json
 
 
 def task() -> str:
-    dict_numbers = {}  # TODO c помощью dict comprehension сформировать словарь
-    return ...  # TODO сеализовать словарь в JSON строку
+    dict_numbers = {key: value for key, value in enumerate(map(lambda x: x**2, range(1, 11)), 1)}  # TODO c помощью dict comprehension сформировать словарь
+    json_string = json.dumps(dict_numbers, indent=4)
+    return json_string  # TODO сеализовать словарь в JSON строку
 
 
 if __name__ == "__main__":
