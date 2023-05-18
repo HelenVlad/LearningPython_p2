eb6f665d711f80040c678d980142843cf694ef47
HelenVG <128369110+HelenVlad@users.noreply.github.com>
2023-05-18 16:59:49 +0500

    Delete PythonPY110 directory

--- PythonPY110/Занятие1/Домашние_задания/task1_1/main.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    def my_enumerate(lst:list):
-        my_enu = zip(range(0, len(lst)), lst)
-        return my_enu
-
-    # a = [15, 5, 6, 4, 8, 79, 16, 3, 0, 3, 5]
-    # for x, y in my_enumerate(a):
-    #     print(x, y)
--- PythonPY110/Занятие1/Лабораторные_задания/task5_2/main.py
+++
@@ -1,8 +0,0 @@
-def task() -> list:
-    temp_tuple = (0, 36.6, 100)
-
-    return ...  # TODO  вернуть список температур по Фаренгейту
-
-
-if __name__ == "__main__":
-    print(task())
